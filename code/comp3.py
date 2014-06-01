c00804ba4748fd9aa21e5eacf45fe6f148baba9c
Bas Westerbaan <bas@westerbaan.name>
2014-06-02 01:05:15 +0200

    code: add comp3

---
+++ code/comp3.py
@@ -0,0 +1,38 @@
+""" Computation 3: check whether the candidate graphs contain unembeddable
+        subgraphs. """
+
+from helper import write_graph6, load_graph6
+from subgraph import find_mono
+
+import binascii
+import json
+
+def main():
+    candidates = []
+    unemb = []
+    print 'Reading candidates ...'
+    with open('../graphs/candidates.g6') as f:
+        for l in f:
+            candidates.append((l[:-1], load_graph6(l[:-1])))
+    print 'Reading minimal unembeddable graphs ...'
+    with open('../graphs/min-unemb-fd3.g6') as f:
+        for l in f:
+            unemb.append((l[:-1], load_graph6(l[:-1])))
+    print 'Checking subgraphs ...'
+    for i, g_pair in enumerate(candidates):
+        gname, g = g_pair
+        if i % 100 == 0:
+            print i, len(candidates)
+        for j, ug_pair in enumerate(unemb):
+            ugname, ug = ug_pair
+            mono = find_mono(ug, g)
+            if mono is None:
+                continue
+            break
+        gsname = binascii.hexlify(gname)
+        with open('../graphs/candidates/%s.json' % gsname, 'w') as f:
+            json.dump({'subgraph': ugname,
+                       'mono': mono}, f)
+
+if __name__ == '__main__':
+    main()
